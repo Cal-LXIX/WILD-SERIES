1ecffda02118817e693f88d6b7e9af0c4a9d705c
Jean-Guy <lyoncal69@hotmail.fr>
2021-06-21 11:26:08 +0200

    quest 3

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,33 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\Response\ResponseStream;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/programs/", name="program_index")
+     */
+
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig',['website' => 'wild series']);
+            
+    }
+
+    
+    /**
+     * @Route("/programs/{id}" , methods={"GET"}, requirements={"page"="\d+"}, name= "program_show")
+     */
+
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id ]);
+    }
+
+}
